782178175c5a9bf3adca7042c350bb4c902b7911
faisal-hdev <itsfaisalchowdhury@gmail.com>
2024-02-13 13:02:31 +0600

    Rectangle calculate functionality emplement are added

--- js/rectangle-area.js
+++ js/rectangle-area.js
@@ -1,3 +1,20 @@
 function calculateRectangleArea() {
-  console.log("rect connected");
+  // get length,width of the triangle
+  const lengthInput = document.getElementById("rectangle-length");
+  const widthInput = document.getElementById("rectangle-width");
+
+  // get length,width value of the triangle
+  const lengthText = lengthInput.value;
+  const widthText = widthInput.value;
+
+  // set to the convert parsFloat value
+  const length = parseFloat(lengthText);
+  const width = parseFloat(widthText);
+
+  // calculate rectangle area
+  const area = length * width;
+
+  // display to the rectangle value
+  const rectangleAreaSpan = document.getElementById("rectangle-area");
+  rectangleAreaSpan.innerText = area;
 }
